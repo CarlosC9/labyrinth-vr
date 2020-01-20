d373815705c6c6ba9b0ab8b31ef7b9d86dd5089f
CarlosC9 <carlosicaraballo1999@gmail.com>
2020-01-20 20:33:23 +0000

    add second scene and modify game1 scene

--- Assets/MyData/Scrpts/startGame.cs
+++ Assets/MyData/Scrpts/startGame.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraTest : MonoBehaviour
+public class startGame : MonoBehaviour
 {
-
-    private Camera cam;
-
+    // Start is called before the first frame update
     void Start()
     {
-        this.cam = GetComponent<Camera>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
